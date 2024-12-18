74be4f2a7830e917a4dadd401f4962d8d2007412
besimray <besimray24@gmail.com>
2024-12-18 18:52:03 +0000

    use meshes

--- lm_eval/tasks/pubmedqa/preprocess_pubmedqa.py
+++ lm_eval/tasks/pubmedqa/preprocess_pubmedqa.py
@@ -1,9 +1,13 @@
 def doc_to_text(doc) -> str:
     ctxs = "\n".join(doc["CONTEXTS"])
 
-    meshes = "\n".join(doc["MESH"])
+    meshes = "\n".join(doc["MESHES"])
 
-    abstract = ctxs + "\n MeSH:" + meshes
+    labels = "\n".join(doc["LABELS"])
+
+    # abstract = ctxs + "\n MeSH:" + meshes + "\n Labels:" + labels
+
+    abstract = "\n MeSHes: " + meshes + "\n Contexts: " + ctxs
     return "Abstract: {}\nQuestion: {}\nAnswer:".format(
         abstract,
         doc["QUESTION"],
